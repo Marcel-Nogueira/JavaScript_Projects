5c757126cadc8d6b44af8de04befff7646e55b5d
Marcel Nogueira <113851295+Marcel-Nogueira@users.noreply.github.com>
2023-09-15 16:36:25 -0300

    Feat:
    Feat: Rodando o Game .
    Add Elementos na Tela
    Nome dos Personagens

--- Projeto_Luta_Funcional/assets/js/script.js
+++ Projeto_Luta_Funcional/assets/js/script.js
@@ -1,9 +1,16 @@
 // === CRIANDO OS GUERREIROS ===
-const char = createKnight ("Guerreiro");
-//const char = createSorcerer ("Mago");
+//const char = createKnight ("Guerreiro");
+const char = createSorcerer ("Mago");
 
 // === CRIANDO OS ADVERSÁRIOS ===
 const monster = createLittleMonster();
 //const monster = createBigMonster ();
 
 
+/// === RODANDO O GAME | Adicionando os elementos na tela===
+stage.start (
+    char,
+    monster,
+    document.querySelector("#char"),
+    document.querySelector("#monster")
+);
