cf38b2c6dc0c1edc96330221a0680e9216ab8768
Marcel Nogueira <113851295+Marcel-Nogueira@users.noreply.github.com>
2023-09-22 22:13:40 -0300

    Feat: Css + JavaScript + Html
    HTML : Adição de Tags + logo marca dos correios
    Css: Adição de mais classes e propriedades 
    JavaScript: Adição das funções para buscar o cep e mostrar na tela

--- API_Correios/assets/js/script.js
+++ API_Correios/assets/js/script.js
@@ -1,3 +1,40 @@
-const cep = document.querySelector ("#cep")
-const result = document.querySelector ("#result")
+function consultarCEP () {
+    let cep = document.querySelector ('#cep').value ;
+
+    if (cep.length !== 8) {
+        alert (`Este CEP ${cep} é invalido`);
+        
+        return;
+    }
+
+    //=== CONSUMINDO API ===
+    const urlConsultaCEP = `https://viacep.com.br/ws/${cep}/json/`;
+
+    fetch (urlConsultaCEP).then (function(response){
+        response.json ().then (function(data){
+            console.log (data);
+            mostrarEndereco(data);
+        })
+    });
+
+};
+
+// === MOSTRAR INFORMAÇÕES NA TELA ===
+function mostrarEndereco(dados) {
+    let result = document.querySelector('#result');
+
+    result.innerHTML = `<p>Endereço: ${dados.logradouro}</p>
+                        <p>Complemento: ${dados.complemento}</p>
+                        <p>Bairro: ${dados.bairro}</p>
+                        <p>UF: ${dados.uf}</p>
+                        <p>Localidade: ${dados.localidade}</p>
+                        <p>DDD: ${dados.ddd}</p>
+                        <p>IBGE: ${dados.ibge}</p>
+                        <p>GIA: ${dados.gia}</p>
+                        <p>SIAFI: ${dados.siafi}</p>`;
+}
+
+
+
+
 
